9c928aa2111b000aa723534ff922f60bb8aa2ec2
Paul <p.d.shelley@me.com>
2026-04-20 16:07:28 -0600

    Async Blink

--- Sources/Application/Application.swift
+++ Sources/Application/Application.swift
@@ -5,15 +5,22 @@
 //  Created by Paul Shelley on 4/17/26.
 //
 
+// MARK: - State
+
+var blinkTimer = PeriodicTimer(interval: 1000)
+var ledState: DigitalValue = .low
+
+// MARK: - Entry Points
 
 func appInit() {
     GPIO.PA02.setDataDirection(.output)
     GPIO.PA02.setValue(.low)
+    blinkTimer.reset()
 }
 
 func appMain() {
-    GPIO.PA02.setValue(.high)
-    delay(1000)
-    GPIO.PA02.setValue(.low)
-    delay(1000)
+    if blinkTimer.hasElapsed() {
+        ledState.toggle()
+        GPIO.PA02.setValue(ledState)
+    }
 }
--- Sources/Support/BridgingHeader.h
+++ Sources/Support/BridgingHeader.h
@@ -11,6 +11,10 @@
 /// Millisecond delay backed by SysTick (delay.c)
 extern void delay(unsigned long ms);
 
+/// Returns the number of milliseconds elapsed since startup, backed by SysTick (delay.c).
+/// Overflows approximately every 49 days.
+extern unsigned long millis(void);
+
 /// Volatile Register Read
 ///
 /// At some point in the future this might be added to Swift, if so this should
